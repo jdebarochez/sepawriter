72c65f9d44cce810ea4715a97a87398c7b00fe56
Calle Hunefalk <hunefalk@gmail.com>
2016-05-31 13:10:18 +0200

    Updated build file

--- SepaWriter/build.bat
+++ SepaWriter/build.bat
@@ -3,21 +3,29 @@ set config=%1
 if "%config%" == "" (
    set config=Release
 )
- 
-set version=1.0.0
+
+set version=
 if not "%PackageVersion%" == "" (
-   set version=%PackageVersion%
+   set version=-Version %PackageVersion%
 )
 
-set nuget=
-if "%nuget%" == "" (
-	set nuget=nuget
-)
+set nunit="tools\nunit\nunit-console.exe"
 
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild SepaWriter.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+REM Build
+"%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" SepaWriter.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+if not "%errorlevel%"=="0" goto failure
 
+REM Unit tests
+%nunit% SepaWriter\SepaWriter.Tests\bin\%config%\SepaWriter.Tests.dll
+if not "%errorlevel%"=="0" goto failure
+
+REM Package
 mkdir Build
-mkdir Build\lib
-mkdir Build\lib\net40
+call %nuget% pack "SepaWriter\SepaWriter.csproj" -symbols -o Build -p Configuration=%config% %version%
+if not "%errorlevel%"=="0" goto failure
 
-%nuget% pack "SepaWriter.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
+:success
+exit 0
+
+:failure
+exit -1
